e0616005068c770521af66b53f0036177df3342a
bedirhanKC <92955596+bedirhanKC@users.noreply.github.com>
2022-02-21 01:26:57 +0300

    Update dersv2_4 mehdi ilani.c

--- ikinci_ogrenme_kayitleri/dersv2_4 mehdi ilani.c
+++ ikinci_ogrenme_kayitleri/dersv2_4 mehdi ilani.c
@@ -2,41 +2,25 @@
 
 int main(){
 	
-	int sayi,sifre,a=3;
-	sifre=3131;
+	int sayi,sifre,hak=3;
+	sifre=1234;
 	
-	printf("Bu program sizi mehdi ilan etmez, ama size Turkiye'de gerceklesmis bir anket sonucunu verir.\n\n");
+	printf("Bu program size Turkiye'de gerceklesmis bir arastirma sonucunu verir.\n\n");
 	
-gir:printf("Ogrenmek icin sifreyi giriniz: ");
+gir:	printf("Ogrenmek icin sifreyi giriniz: ");
 	scanf("%d",&sayi);
 	
 	if(sayi!=sifre){
 		printf("\a\nyanlis giris yaptiniz.\n");
-		a--;
-		if(a==0){
+		hak--;
+		if(hak==0){
 			printf("baska hakkiniz kalmadi, sistem kapatiliyor...");
 			return 0;
 		}
-		printf("%d hakkiniz kaldi\n\n",a);
+		printf("%d hakkiniz kaldi\n\n",hak);
 		goto gir;
 	}
-printf("\n2019da yapilan ankete gore, o donemdeki koyun orani yuzde 51,64'dir");
+printf("\n2019da yapilan arastirmaya gore, o donemdeki koyun orani yuzde 51,64'dir");
 	
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
