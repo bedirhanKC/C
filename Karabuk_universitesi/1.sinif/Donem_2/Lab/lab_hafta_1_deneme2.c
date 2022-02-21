594b1a2ed2307b4b1ef43d1742c175f5191f4216
bedirhanKC <92955596+bedirhanKC@users.noreply.github.com>
2022-02-22 01:04:56 +0300

    Update and rename lab_hafta_1_deneme_2.c to lab_hafta_1_deneme2.c

---
+++ Karabuk_universitesi/1.sinif/Donem_2/Lab/lab_hafta_1_deneme2.c
@@ -0,0 +1,15 @@
+/*
+Bedirhan Kömürcü
+16 şubat 2022
+Lab dersi
+*/
+
+#include<stdio.h>
+
+extern int gid; // global  int deðiþken bildirimi
+
+void fonk(void){
+	
+	printf("deneme2.c gid degisken degeri: %d",gid);
+	
+}
--- Karabuk_universitesi/1.sinif/Donem_2/Lab/lab_hafta_1_deneme_2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-extern int gid; // global  int deðiþken bildirimi
-
-void fonk(void){
-	
-	printf("deneme2.c gid degisken degeri: %d",gid);
-	
-}
