8e6888c5050f518bf7b71c2e529aa6e724bab352
bedirhanKC <92955596+bedirhanKC@users.noreply.github.com>
2022-02-28 21:57:07 +0300

    Add files via upload

---
+++ Karabuk_universitesi/1.sinif/Donem_2/Notlar/recursive_2.c
@@ -0,0 +1,42 @@
+/* 
+Bedirhan Kömürcü 
+28.02.2022 
+09.56 PM 
+*/
+
+#include <stdio.h>
+
+int fun1(int x, int y){
+	
+if (x == 0)
+return y;//base case
+
+else
+return fun1(x - 1, x + y);
+
+}
+
+void fun2(int n){
+	
+int i = 0;
+
+if (n > 1){
+fun2(n - 1);
+printf("\n");
+}
+
+for (i = 0; i < n; i++) //base case – temel durum
+
+printf(" * ");
+
+}
+
+int main(){
+	
+int a = 5, b = 2,c;
+c = fun1(a, b);
+
+printf("Value from the function: %d\n",c);
+fun2(5);
+
+}
