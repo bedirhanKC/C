43d4348239eb69e8f71025cda7d23c9be1fcbfe8
bedirhanKC <92955596+bedirhanKC@users.noreply.github.com>
2022-03-10 22:20:01 +0300

    Add files via upload

---
+++ Karabuk_universitesi/1.sinif/Donem_2/ornek_cozumleri/pointer_dizi_elemanlarinin_toplami.c
@@ -0,0 +1,36 @@
+/*
+Bedirhan Kömürcü
+10.03.2022
+22.09
+*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int dizi[10],n,i,toplam=0;
+	int *pdizi,*pn,*ptoplam;
+	
+	pdizi=dizi;
+	pn=&n;
+	ptoplam=&toplam;
+	
+	printf(" Bir arraydeki tum elemanlari topla :\n------------------------------------------------\n");
+	
+	printf("eleman sayisini giriniz:");
+	scanf("%d",pn);
+	
+	for(i=0 ; i<(*pn) ; i++){
+		
+		printf("%d. elemani giriniz:",i+1);
+		scanf("%d",(pdizi+i));
+		
+		(*ptoplam)+=(*(pdizi+i));
+		
+	}
+	
+	printf("tum elemanlarin toplami : %d",*ptoplam);
+	
+	
+	return 0;
+}
