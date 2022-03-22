08042f222e4b834e111c4f54f77be6effa99ee9c
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-03-22 23:17:33 +0300

    Add files via upload

---
+++ Karabuk_universitesi/1.sinif/Donem_2/ornek_cozumleri/example_student_number_to_binary.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	
+	int ogrenciNumarasi;
+	int i=0,j;
+	
+	int *ptr=(int *)malloc(sizeof(int));
+	
+	printf("ogrenci numaranizi giriniz : ");
+	scanf("%d",&ogrenciNumarasi);
+	
+	while(1){
+		
+		if(ogrenciNumarasi<1) break;
+		
+		*(ptr+i)=ogrenciNumarasi%2;
+		ogrenciNumarasi=ogrenciNumarasi/2;
+		
+		i++;
+		
+		ptr=realloc(ptr,(1+i)*sizeof(int));
+		
+	}
+	
+	
+	for(j=i;j>0;j--){
+		
+		printf("%d",*(ptr+j-1));
+	}
+	
+}
