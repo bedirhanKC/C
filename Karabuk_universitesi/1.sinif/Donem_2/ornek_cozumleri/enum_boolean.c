a3e487da50d4925d5e98551b946159d006bd0409
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-04-12 23:20:29 +0300

    Add files via upload

---
+++ Karabuk_universitesi/1.sinif/Donem_2/ornek_cozumleri/enum_boolean.c
@@ -0,0 +1,24 @@
+/*
+Bedirhan Kömürcü
+12.04.2022
+23.06
+*/
+#include<stdio.h>
+
+int main(){
+	
+	enum boolean{
+		
+		false=0,
+		true=1
+		
+		
+	}istrue;
+	
+	istrue=true;
+	
+	if(istrue==true) printf("true \n");
+	
+	
+	return 0;
+}
