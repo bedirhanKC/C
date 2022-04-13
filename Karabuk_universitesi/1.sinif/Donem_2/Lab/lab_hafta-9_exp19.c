ff5a2253d9095576355399b5fc607d2c9cc71c34
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-04-13 19:37:25 +0300

    Add files via upload

---
+++ Karabuk_universitesi/1.sinif/Donem_2/Lab/lab_hafta-9_exp19.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+struct student{
+	
+	char name[50];
+	int age;
+	
+};
+
+struct student getInformation();
+
+main(){
+	
+	
+	struct student s;
+	
+	s=getInformation();
+	
+	printf("\nDisplaying information \n");
+	printf("name: %s",s.name);
+	printf("\nroll : %d",s.age);
+	
+}
+
+struct student getInformation(){
+	
+	struct student s1;
+	
+	printf("enter name : ");
+	scanf("%[^\n]%*c",s1.name);
+	
+	printf("enter age : ");
+	scanf("%d",&s1.age);
+	
+	return s1;
+}
