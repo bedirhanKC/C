f46a3b687a6af0ee2ac33977b7ce5eeeab72510e
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-04-13 19:38:06 +0300

    Add files via upload

---
+++ Karabuk_universitesi/1.sinif/Donem_2/Lab/lab_hafta-9_exp17.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+struct player{
+	
+	char pname[20];
+	
+}p1;
+
+char* play(struct player *temp_p1){
+	
+	
+	strcpy(temp_p1->pname,"kohli");
+	return temp_p1->pname;
+	
+}
+
+main(){
+	
+	
+	strcpy(p1.pname,"dhoni");
+	printf("%s %s",p1.pname,play(&p1));
+	
+}
+
+
