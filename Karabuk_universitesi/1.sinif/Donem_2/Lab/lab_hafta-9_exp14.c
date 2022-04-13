b12804fb9ba1409dce65f11721838b4a6d712636
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-04-13 19:36:50 +0300

    Add files via upload

---
+++ Karabuk_universitesi/1.sinif/Donem_2/Lab/lab_hafta-9_exp14.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+struct toplama{
+	
+	int sayi1;
+	int sayi2;
+	
+}toplama;
+
+main(){
+	
+	printf("1. sayiyi giriniz:");
+	scanf("%d",&toplama.sayi1);
+	printf("2. sayiyi giriniz:");
+	scanf("%d",&toplama.sayi2);
+	
+	printf("toplam:%d",toplama.sayi1+toplama.sayi2);
+	
+}
