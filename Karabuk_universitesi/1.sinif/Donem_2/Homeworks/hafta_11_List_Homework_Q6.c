9c01d3de4af060e1cb84562ab81f24dbb9f46ec1
Bedirhan Kömürcü <kmrc.bedirhan@yandex.com>
2022-05-05 22:54:42 +0300

    Add files via upload

---
+++ Karabuk_universitesi/1.sinif/Donem_2/Homeworks/hafta_11_List_Homework_Q6.c
@@ -0,0 +1,90 @@
+/*
+Bedirhan Kömürcü
+05.05.2022
+22.50
+List Homework - Q6
+*/
+
+/*
+Klavyeden -1 girilene kadar verilen çift sayýlarý tek baðlý doðrusal bir listenin baþýna
+verilen tek sayýlarý ise listenin sonuna ekleyen programý yazýnýz.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	
+	int number;
+	struct node *next;
+	
+	
+};
+
+typedef struct node node;
+
+int main(){
+	
+	
+	int deger,TekSayisi=0,CiftSayisi=0; 
+	node *thead,*p,*chead,*q;
+	
+	while(1){
+		
+		printf("Bir deger giriniz:");
+		scanf("%d",&deger);
+		
+		if(deger == -1) break;
+		
+		if(deger%2!=0){ //Bu blokta tek sayýlar ile liste oluþturma iþlemi gerçekleþir.
+			
+			if(TekSayisi==0){
+			
+				thead=(node*)malloc(sizeof(node));
+				p=thead;
+	
+			}else{
+			
+				p->next=(node*)malloc(sizeof(node));
+				p=p->next;			
+			}
+		
+			p->number=deger;
+			TekSayisi++;
+			p->next=NULL;
+						
+		}else{ //Bu blokta da cift sayýlarý yeni bir liste gibi olusturma islemi gerçekleþir.
+			
+			if(CiftSayisi==0){
+			
+				chead=(node*)malloc(sizeof(node));
+				q=chead;
+	
+			}else{
+			
+				q->next=(node*)malloc(sizeof(node));
+				q=q->next;			
+			}
+		
+			q->number=deger;
+			CiftSayisi++;
+			q->next=NULL;
+					
+		}
+	
+	}
+	
+	q->next=thead; // Bu satýrda, cift sayýlar listesinin sonuna tek sayilar listesi eklenmiþ olur.
+	
+	p=chead;
+	
+	while(p!=NULL){ //Bu döngünün içerisinde de en son yazdýrma iþlemi gerçekleþir.
+		
+		printf("%d  ",p->number);
+		
+		p=p->next;
+		
+	}
+	
+return 0;	
+}
